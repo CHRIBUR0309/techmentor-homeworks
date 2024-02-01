51695051a9b748fbee2a88fc01545f0575023af1
CHRIBUR0309 <pikatyupokenonnori@gmail.com>
2024-02-01 10:11:50 +0900

    リスタート。

---
+++ todo/spring_boot/restful_todo/src/main/java/com/example/restful_todo/RestfulTodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.restful_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulTodoApplication.class, args);
+	}
+
+}
---
+++ todo/spring_boot/restful_todo/src/test/java/com/example/restful_todo/RestfulTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restful_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
