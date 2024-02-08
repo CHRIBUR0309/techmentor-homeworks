57e34310e518a745ef45b0abab665e7d78880c79
CHRIBUR0309 <pikatyupokenonnori@gnail.com>
2024-02-08 10:23:13 +0900

    GETだけ動かない。

--- todo/spring_boot/restful_todo/src/main/java/com/example/restful_todo/controller/TodoController.java
+++ todo/spring_boot/restful_todo/src/main/java/com/example/restful_todo/controller/TodoController.java
@@ -1,70 +1,46 @@
 package com.example.restful_todo.controller;
 
-import org.springframework.stereotype.*;
-import org.springframework.ui.*;
+import java.util.*;
 import org.springframework.web.bind.annotation.*;
 import com.example.restful_todo.model.*;
 import com.example.restful_todo.service.*;
 import lombok.*;
 
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class TodoController {
     private final TodoItemService todoItemService;
 
-    @GetMapping("/")
-    public String topPage() {
-        return "index";
-    }
-
     @GetMapping("/get")
-    public String getAllItems(Model model) {
-        model.addAttribute("todoItems", todoItemService.getAllItems());
-        return "todo_item";
+    public Collection<TodoItem> getAllItems() {
+        return todoItemService.getAllItems();
     }
 
     @GetMapping("/get/{todoId}")
-    public String getItem(@PathVariable long todoId, Model model) {
-        model.addAttribute("todoId", todoId);
-        model.addAttribute("todoItem", todoItemService.getItem(todoId));
-        return "todo_item";
+    public TodoItem getItem(@PathVariable long todoId) {
+        return todoItemService.getItem(todoId);
     }
 
     @PostMapping("/post/{title}/{status}")
-    public String insertItemWithoutDetails(@PathVariable String title, @PathVariable String status,
-            Model model) {
-        model.addAttribute("title", title);
-        model.addAttribute("status", status);
+    public void insertItemWithoutDetails(@PathVariable String title, @PathVariable String status) {
         todoItemService.insertItem(new TodoItem(0, title, status, ""));
-        return "redirect:/";
     }
 
     @PostMapping("/post/{title}/{status}/{details}")
-    public String insertItemWithDetails(@PathVariable String title, @PathVariable String status,
-            @PathVariable String details, Model model) {
-        model.addAttribute("title", title);
-        model.addAttribute("status", status);
-        model.addAttribute("details", details);
+    public void insertItemWithDetails(@PathVariable String title, @PathVariable String status,
+            @PathVariable String details) {
         todoItemService.insertItem(new TodoItem(0, title, status, details));
-        return "redirect:/";
     }
 
-    @PostMapping("/patch/{todoId}/{title}/{status}/{details}")
-    public String updateItem(@PathVariable long todoId, @PathVariable String title,
-            @PathVariable String status, @PathVariable String details, Model model) {
-        model.addAttribute("todoId", todoId);
-        model.addAttribute("title", title);
-        model.addAttribute("status", status);
-        model.addAttribute("details", details);
+    @PatchMapping("/patch/{todoId}/{title}/{status}/{details}")
+    public void updateItem(@PathVariable long todoId, @PathVariable String title,
+            @PathVariable String status, @PathVariable String details) {
         todoItemService.updateItem(new TodoItem(todoId, title, status, details));
-        return "redirect:/";
     }
 
-    @PostMapping("/delete/{todoId}")
-    public String updateItem(@PathVariable long todoId, Model model) {
-        model.addAttribute("todoId", todoId);
+    @DeleteMapping("/delete/{todoId}")
+    public void updateItem(@PathVariable long todoId) {
         todoItemService.deleteItem(todoId);
-        return "redirect:/";
     }
 }
